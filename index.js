7d9fd929449d72ffdfcdd2a574a356067d998871
Ivan-Belenkov <ioann.iv.grozny@gmail.com>
2016-10-09 12:11:02 +0300

    entry point carcass

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
  * Created by Ivan on 09.10.2016.
  */
 
+const postcss = require("postcss");
+
+module.exports = postcss.plugin("postcss-modal", plugin);
+
+function plugin() {
+    return function(css, result) {
+
+    }
+}

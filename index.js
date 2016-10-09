b4f251edd726499247076727a8920d7c102661e3
Ivan-Belenkov <ioann.iv.grozny@gmail.com>
2016-10-09 12:36:17 +0300

    add defaults

--- index.js
+++ index.js
@@ -2,12 +2,49 @@
  * Created by Ivan on 09.10.2016.
  */
 
-const postcss = require("postcss");
+var postcss = require("postcss");
+
+var defaults = {};
+
+defaults.container = {
+    "display": "block",
+    "position": "fixed",
+    "top": "0",
+    "right": "0",
+    "bottom": "0",
+    "left": "0",
+    "z-index": "2147483646",
+    "overflow": "hidden",
+    "white-space": "nowrap",
+    "font-size": 0,
+    "text-align": "center"
+};
+
+defaults.pseudo = {
+    "content": "\"\"",
+    "display": "inline-block",
+    "vertical-align": "middle",
+    "height": "100%",
+    "width": "0"
+};
+
+defaults.child = {
+    "display": "inline-block",
+    "box-sizing": "border-box",
+    "vertical-align": "middle",
+    "max-height": "100%",
+    "overflow-x": "hidden",
+    "overflow-y": "auto"
+};
 
 module.exports = postcss.plugin("postcss-modal", plugin);
 
 function plugin() {
     return function(css, result) {
+        css.walkDecls("display", function(decl) {
+            if (decl.value !== "modal") return;
 
+
+        });
     }
 }
